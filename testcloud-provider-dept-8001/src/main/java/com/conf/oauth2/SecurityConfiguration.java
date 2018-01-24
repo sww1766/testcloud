c7bc63f921a71619e8ec5a6f5770b2c3c02f08f7
admin <sww1766@163.com>
2018-01-24 22:26:42 +0800

    注释Oauth2
    (cherry picked from commit 7941181)

--- testcloud-provider-dept-8001/src/main/java/com/conf/oauth2/SecurityConfiguration.java
+++ testcloud-provider-dept-8001/src/main/java/com/conf/oauth2/SecurityConfiguration.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.conf.oauth2;
 //
 //
@@ -23,6 +24,45 @@
 ////                .withUser("user_2").password("123456").authorities("USER");
 ////    }
 //
+=======
+package com.conf.oauth2;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user_1").password("123456").authorities("USER")
+//                .and()
+//                .withUser("user_2").password("123456").authorities("USER");
+//    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user_1").password("123456").authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password("123456").authorities("USER").build());
+        return manager;
+    }
+
+    /**
+     * 这一步的配置是必不可少的，否则SpringBoot会自动配置一个AuthenticationManager,覆盖掉内存中的用户
+     */
+>>>>>>> 7941181... 注释Oauth2
 //    @Bean
 //    @Override
 //    protected UserDetailsService userDetailsService(){
